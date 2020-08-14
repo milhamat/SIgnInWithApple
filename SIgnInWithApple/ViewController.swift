611923270205988ea8d2768da5ebc243d6255d51
Mas Rasid <rasid517@gmail.com>
2020-08-14 09:25:54 +0700

    Initial Commit

---
+++ SIgnInWithApple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SIgnInWithApple
+//
+//  Created by Muhammat Rasid Ridho on 14/08/20.
+//  Copyright © 2020 Muhammat Rasid Ridho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
