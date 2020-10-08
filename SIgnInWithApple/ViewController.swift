480350e4a1c33ba8a997a25b07e87d7430240c5a
MilhamAt <M477ashiddiq@gmail.com>
2020-10-08 14:57:18 +0700

    first commit

--- SIgnInWithApple/ViewController.swift
+++ SIgnInWithApple/ViewController.swift
@@ -7,14 +7,79 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var AppleIDLabel : UILabel!
+    @IBOutlet weak var firstNameLabel : UILabel!
+    @IBOutlet weak var lastNameLabel : UILabel!
+    @IBOutlet weak var emailIdLabel : UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpAppleSignBtn()
+
     }
-
-
+    
+    func setUpAppleSignBtn() {
+        let signInBtn = ASAuthorizationAppleIDButton()
+        signInBtn.frame = CGRect (x: 20, y: (UIScreen.main.bounds.size.height - 170), width: (UIScreen.main.bounds.size.width - 40), height: 50)
+        signInBtn.addTarget(self, action: #selector(signInActionBtn), for: .touchUpInside)
+        
+        self.view.addSubview(signInBtn)
+    }
+    
+    @objc func signInActionBtn() {
+        //    print ("Apple Sign In")
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.email, .fullName]
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+        
+    }
 }
 
+
+extension ViewController : ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        let alert = UIAlertController (title: "Error", message: "\(error.localizedDescription)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+        case let credentials as ASAuthorizationAppleIDCredential:
+            
+            print(credentials.user)
+            print(credentials.email!)
+            print(credentials.fullName!)
+            
+            self.emailIdLabel.text = credentials.email
+            self.firstNameLabel.text = credentials.fullName?.givenName
+            self.lastNameLabel.text = credentials.fullName?.familyName
+            self.AppleIDLabel.text = credentials.user
+            
+        
+        case let credentials as ASPasswordCredential:
+            print(credentials.password)
+            
+        default:
+            let alert = UIAlertController (title: "Apple Sign In", message: "Something wrong with your Apple Sign In", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+            present(alert, animated: true, completion: nil)
+            
+        }
+    }
+    
+}
+
+extension ViewController : ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+    return view.window!
+    }
+    
+}
